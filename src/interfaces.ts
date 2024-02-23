e96b98f70effb44912588563ff2b84ff51458235
Erik Noorland <noorlandjr_e@hotmail.com>
2024-02-23 09:05:12 +0100

    Consistent type annotations

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,6 +1,6 @@
 export interface Options {
-  maxIterations?:number,
-  tolerance?:number,
+  maxIterations?: number,
+  tolerance?: number,
 }
 
 export interface Point {
@@ -28,15 +28,15 @@ export interface Transformation {
 };
 
 export interface Result {
-  transformation:Transformation,
-  dataPoints:Array<Point>,
-  numIterations:number,
+  transformation: Transformation,
+  dataPoints: Array<Point>,
+  numIterations: number,
 };
 
-export const instanceOfPoint = (object:any): object is Point => {
+export const instanceOfPoint = (object: any): object is Point => {
   return 'x' in object && 'y' in object;
 }
 
-export const instanceOfLineSegment = (object:any): object is LineSegment => {
+export const instanceOfLineSegment = (object: any): object is LineSegment => {
   return 'x1' in object && 'x2' in object && 'y1' in object && 'y2' in object;
 }
